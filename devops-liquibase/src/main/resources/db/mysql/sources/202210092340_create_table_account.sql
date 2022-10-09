ef6c09afbe21bc289f57dd119859c06393f6bd16
minhnh22@onemount.com <minhnh22@onemount.com>
2022-10-09 23:58:54 +0700

    commit config k8s

---
+++ devops-liquibase/src/main/resources/db/mysql/sources/202210092340_create_table_account.sql
@@ -0,0 +1,21 @@
+-- auto-generated definition
+create table accounts
+(
+    id         bigint auto_increment
+        primary key,
+    user_id    int                                null,
+    username   varchar(255)                       not null,
+    password   varchar(255)                       not null,
+    status     varchar(255)                       null,
+    created_at datetime default CURRENT_TIMESTAMP null,
+    created_by varchar(255)                       null,
+    updated_at datetime                           null on update CURRENT_TIMESTAMP,
+    updated_by varchar(255)                       null,
+    deleted_at datetime                           null,
+    constraint account_username_idx
+        unique (username)
+);
+
+create index account_user_id_idx
+    on accounts (user_id);
+
